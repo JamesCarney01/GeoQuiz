6131bc03686ba85e3f668601b5af3880b19e46a3
JamesCarney01 <93592851+JamesCarney01@users.noreply.github.com>
2022-08-17 23:52:19 +1000

    Some revisions made, current "manifest merger failed" error unresolved. Possible solution found here:  https://stackoverflow.com/questions/67412084/android-studio-error-manifest-merger-failed-apps-targeting-android-12
    https://stackoverflow.com/questions/67654506/manifest-merger-failed-targeting-android-12/67668116#67668116
    
    Changes include addition of Toast features in Str and cleanup of structure in MainActivity.kt

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,46 +1,30 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
+import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
-    private var currentIndex = 0
-
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-
+        setContentView(R.layout.activity_main)
+        trueButton.setOnClickListener { view: View ->
             Toast.makeText(
                 this,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT
             ).show()
         }
-
-
-
+        falseButton.setOnClickListener { view: View ->
             Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
             ).show()
         }
-
     }
 }
